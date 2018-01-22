447d70042707721ea0420334b3239bba110869ba
Hilton Adolinama <hiltonadolinama@Hiltons-MacBook-Pro.local>
2018-01-21 19:47:29 -0700

    Landing Page

---
+++ assets/Js/index.js
@@ -0,0 +1,31 @@
+console.log("Connected and Alive!");
+
+// DATES
+var dateMatches = {
+        peter:  [ "Peter Brown", "https://images.unsplash.com/photo-1463453091185-61582044d556?auto=format&fit=crop&w=2550&q=80" ],
+        kelvin: ["Kelvin Fish", "https://images.unsplash.com/photo-1485274466491-6c0baa1cfc2a?auto=format&fit=crop&w=1650&q=80" ],
+        john:   [ "John Smith", "https://images.unsplash.com/photo-1508746829417-e6f548d8d6ed?auto=format&fit=crop&w=1650&q=80" ],
+        magaret:[ "Margaret Poppins", "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&w=2434&q=80" ],
+        regina: [ "Regina Lee", "https://images.unsplash.com/photo-1439778615639-28529f7628bc?auto=format&fit=crop&w=2434&q=80" ],
+        Phlipe: [ "Phlipe Francois", "https://images.unsplash.com/photo-1507399076055-c021699485c3?auto=format&fit=crop&w=1332&q=80" ],
+        david:  [ "David Motombo", "https://images.unsplash.com/photo-1510271239234-acaa528210e5?auto=format&fit=crop&w=668&q=80" ],
+        elle:   [ "Elle Lepetite", "https://images.unsplash.com/photo-1507101105822-7472b28e22ac?auto=format&fit=crop&w=2552&q=80" ],
+        cruz:   [ "Cruz d'Amigo", "https://images.unsplash.com/photo-1513313226122-f18f0b81f99e?auto=format&fit=crop&w=2304&q=80" ],
+        nana:   [ "Nana Ama", "https://images.unsplash.com/photo-1505891964375-ec59cc95a9e5?auto=format&fit=crop&w=2434&q=80" ]
+}
+
+
+
+$(document).ready(function() {
+  $('button').click(function() {
+    console.log("I have been clicked");
+
+
+
+            // Then initiate a loop to generate 9 separate numbers
+            for (var i = 0; i < dateMatches.length; i++) {
+              alert("Your Matche is ");
+            }
+  });
+
+});
